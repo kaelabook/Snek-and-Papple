8e97e4611f16e27154f2909bb851212923d11f86
Makaela Sellers <117038602+kaelabook@users.noreply.github.com>
2023-03-20 16:51:31 -0400

    Update SnekGame.java

--- SnekGame.java
+++ SnekGame.java
@@ -1,5 +1,7 @@
-```public class SnekGame {
+// MAIN CLASS
+public class SnekGame {
     public static void main(String[] args) {
+        // THE FOLLOWING LINE IS FOR REDUCING LAG
         System.setProperty("sun.java2d.openg1", "true");
         new GameFrame().GameMenu();
-    }```
+    }
